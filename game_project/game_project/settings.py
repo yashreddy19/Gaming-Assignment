082a083eba5bcea6f317a4120d77d5441c5869db
Yashwant Reddy <reddyyash19@gmail.com>
2023-07-19 20:24:57 +0530

    change

--- game_project/game_project/settings.py
+++ game_project/game_project/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = "django-insecure-k4cgo0*j8+uoay516ibfs9m-p#2x6!yt4ymnjd=rjgp707twax
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Application definition
 
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = "game_project.urls"
