e51bde884cbd85fa48b413e853c370cce8b038e5
Yashwant Reddy <reddyyash19@gmail.com>
2023-07-19 19:55:41 +0530

    change

--- game_project/gaming_app/urls.py
+++ game_project/gaming_app/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from .views import GameViewSet
-
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Game CRUD API",
+      default_version='v1',
+      description="API for managing game records, allowing CRUD operations to create, retrieve, update and delete game information.",
+      contact=openapi.Contact(email="reddyyash19@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path("game/", GameViewSet.as_view({"post": "create"})),
     path(
@@ -10,4 +23,10 @@ urlpatterns = [
         ),
     ),
     path("games/", GameViewSet.as_view({"get": "list"})),
+]
+
+#swagger UI
+urlpatterns += [
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
