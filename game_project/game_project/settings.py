81023f61a2ef17239e98fc8bed24ac6848db8838
Yashwant Reddy <reddyyash19@gmail.com>
2023-07-19 19:10:13 +0530

    change

--- game_project/game_project/settings.py
+++ game_project/game_project/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,14 +79,7 @@ WSGI_APPLICATION = "game_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "argus",
-        "USER": "projectuser",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": 5433,
-    }
+    "default": dj_database_url.parse((os.environ.get("DATABASE_URL"))),
 }
 
 
